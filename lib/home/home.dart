b69cdb15d963509f0507c0c5638b246621a13220
3284#mudar <daviondejesus@gmail.com>
2020-02-17 07:21:15 -0300

    update

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -21,7 +21,7 @@ class _HomeState extends State<Home> {
 
   @override
   void initState() {
-    // TODO: implement initState
+    
     super.initState();
     _readData().then((data){
       setState(() {
